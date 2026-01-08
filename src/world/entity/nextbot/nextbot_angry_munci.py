cab2e9a66160743a194e1f2b64c6cbbd7822bf9f
angrymuncifan2888 <angrymuncifan2888@gmail.com>
2026-01-09 00:43:54 +0100

    Buff Angry Munci and Super Munci))

--- src/world/entity/nextbot/nextbot_angry_munci.py
+++ src/world/entity/nextbot/nextbot_angry_munci.py
@@ -1,10 +1,10 @@
 from .nextbot import EntityNextbot
-from assets import SoundStorage, AdvancedSound, calculate_sound_volume
+from assets import SoundStorage, AdvancedSound
 
 
 class NextbotAngryMunci(EntityNextbot):
     def __init__(self, world, position, creation_params=None):
-        super().__init__(world, position, 120, 120, AdvancedSound(SoundStorage.ANGRY_MUNCI_AMBIENCE), 1200, 2, creation_params)
+        super().__init__(world, position, 120, 120, AdvancedSound(SoundStorage.ANGRY_MUNCI_AMBIENCE), 1500, 2, creation_params)
 
     def update(self, delta: float):
         super().update(delta)
--- src/world/entity/nextbot/nextbot_super_munci.py
+++ src/world/entity/nextbot/nextbot_super_munci.py
@@ -1,9 +1,9 @@
 from .nextbot import EntityNextbot
-from assets import SoundStorage, AdvancedSound, calculate_sound_volume
+from assets import SoundStorage, AdvancedSound
 
 class NextbotSuperMunci(EntityNextbot):
     def __init__(self, world, position, creation_params=None):
-        super().__init__(world, position, 120, 120, AdvancedSound(SoundStorage.SUPER_MUNCI_AMBIENCE), 5000, 1.5, creation_params)
+        super().__init__(world, position, 120, 120, AdvancedSound(SoundStorage.SUPER_MUNCI_AMBIENCE), 9000, 1.5, creation_params)
 
     def update(self, delta: float):
         super().update(delta)
