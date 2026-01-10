c8f879d620176af865d2f3874a444470f29804c5
angrymuncifan2888 <angrymuncifan2888@gmail.com>
2026-01-10 22:01:04 +0100

    Asya can now die to bullets

--- src/world/nextbot/nextbot_asya.py
+++ src/world/nextbot/nextbot_asya.py
@@ -1,10 +1,16 @@
 from .entity_nextbot import EntityNextbot
 from assets import SoundStorage, AdvancedSound, Sprites
+from ..components import HasHealth
 
 
-class NextbotAsya(EntityNextbot):
+class NextbotAsya(EntityNextbot, HasHealth):
     @classmethod
     def image(self):
         return Sprites.ASYA_TEXTURE
     def __init__(self, world, position, creation_params=None):
         super().__init__(world, position, 100, 120, AdvancedSound(SoundStorage.ASYA_AMBIENCE), 300, 2, creation_params)
+        self.max_hp = 10
+        self.hp = 10
+    
+    def on_death(self):
+        self.destroy()
